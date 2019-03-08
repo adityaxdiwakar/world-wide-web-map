d7e3686fddc0b44194b469ba7ef9dc72346f5093
Aditya Diwakar <i@adityadiwakar.me>
2019-03-07 21:30:33 -0500

    add more final patches

--- main.py
+++ main.py
@@ -6,51 +6,75 @@ import threading
 ROOT_URL = "https://google.com"
 REGEX_URL = "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
 
-urls_to_visit = json.load(open("to_visit.json", "r"))
-urls_visited = json.load(open("visited.json", "r"))
-
-
-
 from lxml import html
+
+def get_visited():
+    return json.load(open("visited.json", "r"))
+
+def get_to_visit():
+    return json.load(open("to_visit.json", "r")) 
+
+def push_visited(data):
+    json.dump(data, 
+                       open("visited.json", "w"),
+                       indent = 1)
+
+def push_to_visit(data):
+    json.dump(data, 
+                       open("to_visit.json", "w"),
+                       indent = 1)
+
+def append_to_visit(url):
+    to_visit = get_to_visit()
+    to_visit.append(url)
+    push_to_visit(to_visit)
+
+def append_visited(url):
+    visited = get_visited()
+    visited.append(url)
+    push_visited(visited)
+
+def check_for_visitation(url):
+    if url in get_visited():
+        return False
+    return True
+
+def check_for_queued_visit(url):
+    if url in get_to_visit():
+        return False
+    return True
+
+def queue_pop(index):
+    to_visit = get_to_visit()
+    to_visit.pop(index)
+    push_to_visit(to_visit)
 
 page = requests.get(ROOT_URL)
 webpage = html.fromstring(page.content)
 finds = webpage.xpath('//a/@href')
 finds = [x for x in finds if x.startswith("https")]
-urls_to_visit += finds
-urls_visited.append(ROOT_URL)
+for url in set(list(finds)):
+    append_to_visit(url)
+append_visited(ROOT_URL)
 
-def check_url(index):
-    while True:
-        t_url = urls_to_visit[index]
-        print(f"[{len(urls_visited)}] Visiting {t_url} and found...", end=" ")
-        page = requests.get(t_url)
-        webpage = html.fromstring(page.content)
-        finds = webpage.xpath('//a/@href')
-        print(f"{len(finds)} URLs, total: {len(urls_to_visit)}")
-        finds = [x for x in finds if x.startswith("https")]
-        for url in finds:
-            if url not in urls_visited:
-                urls_to_visit.append(url)
-        urls_visited.append(t_url)
-        urls_to_visit.pop(index)
-
-
-thread1 = threading.Thread(target=check_url, args=[0])
-thread2 = threading.Thread(target=check_url, args=[-1])
-thread1.start()
-thread2.start()
+print(get_to_visit())
+print(get_visited())
 
 while True:
-    if len(urls_visited) % 50 == 0:
-        urls_to_visit = list(set(urls_to_visit))
-        json.dump(
-            urls_to_visit,
-            open("to_visit.json", "w"),
-            indent =4
-        )
-        json.dump(
-            urls_visited,
-            open("visited.json", "w"),
-            indent = 4
-        )
+    t_url = get_to_visit()[0]
+
+    print(f"[{len(get_visited())}] Visiting {t_url} and found...", end=" ")
+    page = requests.get(t_url)
+    webpage = html.fromstring(page.content)
+    finds = webpage.xpath('//a/@href')
+    print(f"{len(finds)} URLs, total: {len(get_to_visit())}")
+
+    finds = [x for x in finds if x.startswith("https")]
+
+    for url in set(list(finds)):
+        if check_for_queued_visit(url):
+            if check_for_visitation(url):
+                append_to_visit(url)
+
+    append_visited(t_url)
+    queue_pop(0)
